9577ffe7adfbcc4ca39314ba5ff2fd731b38ec3f
ORG <47654977+ryeongee@users.noreply.github.com>
2021-02-06 21:50:59 +0900

    BOJ update

--- Baekjoon Online Judge/BOJ (1931) - 회의실 배정.py
+++ Baekjoon Online Judge/BOJ (1931) - 회의실 배정.py
@@ -1,18 +1,13 @@
-N = int(input()) #회의실 갯수
-plan = []
+num = int(input())
+t_list = []
+for i in range(num):
+    t_list.append(list(map(int,input().split())))
+t_list = sorted(t_list, key=lambda a: a[0])
+t_list = sorted(t_list, key=lambda a: a[1])
 count = 0
-save = []
-
-for i in range(N):
-    item = input().split(" ") # i 번째 회의
-    item = list(map(int,item)) # 정수로 변환
-    plan.append([item[0], item[1]])  #시작시간, 종료시간
-plan.sort() # 오름차순 정렬
-save = plan
-for i in range(N):
-    save.pop(i) #기준 list 제거
-    for j in range(1,N-1):
-        if plan[i][0]<=plan[i+j][0] and plan[i+1][1]<=plan[i][1]: #범위안에있으면
-            save.remove(plan[i+j]) #제거
-
-print(save)
+list_end = 0
+for i, j in t_list:
+    if i >= list_end:
+        count += 1
+        list_end = j
+print(count)
