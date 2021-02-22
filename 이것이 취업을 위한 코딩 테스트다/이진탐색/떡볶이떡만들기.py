628e0de19992c3e64d49aba6c3ef803fd9719be2
ORG <47654977+ryeongee@users.noreply.github.com>
2021-02-22 22:13:03 +0900

    s

--- 이것이 취업을 위한 코딩 테스트다/이진탐색/떡볶이떡만들기.py
+++ 이것이 취업을 위한 코딩 테스트다/이진탐색/떡볶이떡만들기.py
@@ -1,1 +1,13 @@
 n, m = map(int,input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+start = 0
+end = arr[n-1]
+mid = (start+end)//2
+while start<=end:
+    for x in arr:
+        if x>mid:
+            mid = (set+end)//2
+            start -1
+        else:
+            set
