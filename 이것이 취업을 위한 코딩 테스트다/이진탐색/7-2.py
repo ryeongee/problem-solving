b71f0a92dec493d6ca3ad9544ea99dfa4c37939d
ORG <47654977+ryeongee@users.noreply.github.com>
2021-03-01 15:16:02 +0900

    s

--- 이것이 취업을 위한 코딩 테스트다/이진탐색/7-2.py
+++ 이것이 취업을 위한 코딩 테스트다/이진탐색/7-2.py
@@ -1,10 +1,11 @@
 def bin_search(start,end,array,target):
     if start > end:
         return None
-    mid = (start+end)//2
+    mid = (start+end)//2;
     if array[mid] == target:
         return mid;
     elif array[mid]> target:
         return bin_search(start,mid-1,array,target);
     else:
         return bin_search(mid+1,end,array,target);
+#sss
