46f597fc3f6ec6e13710f7ba5e48d76f3275b89e
ORG <47654977+ryeongee@users.noreply.github.com>
2021-03-01 15:16:02 +0900

    s

--- 이것이 취업을 위한 코딩 테스트다/이진탐색/7-5.py
+++ 이것이 취업을 위한 코딩 테스트다/이진탐색/7-5.py
@@ -4,7 +4,7 @@ while start <= end:
 mid = (start + end) // 2
 # 찾은 경우 중간점 인덱스 반환
 if array[mid] = = target:
-return mid
+return mid;
 # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
 elif array[mid] > target:
 end = mid - 1
