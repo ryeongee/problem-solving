1e6d7c6eda74914f50f8b776489c668ff5f97076
ORG <47654977+ryeongee@users.noreply.github.com>
2021-04-02 18:49:55 +0900

    .

--- 이것이 취업을 위한 코딩 테스트다/최단경로/ex3.py
+++ 이것이 취업을 위한 코딩 테스트다/최단경로/ex3.py
@@ -2,19 +2,18 @@ import heapq
 import sys
 input = sys.stdin.readline
 INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
-# 노드의 개수, 간선의 개수를 입력받기
-n, m = map(int, input().split())
-# 시작 노드 번호를 입력받기
-start = int(input())
+# 노드의 개수, 간선의 개수, 시작 노드 번호를 입력받기
+n, m, c = map(int, input().split())
+
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
 graph = [[] for i in range(n + 1)]
 # 최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF] * (n + 1)
 # 모든 간선 정보를 입력받기
 for _ in range(m):
-    a, b, c = map(int, input().split())
+    x, y, z = map(int, input().split())
     # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
-    graph[a].append((b, c))
+    graph[x].append((y, z))
 def dijkstra(start):
     q = []
     # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
@@ -34,12 +33,13 @@ def dijkstra(start):
                 distance[i[0]] = cost
                 heapq.heappush(q, (cost, i[0]))
 # 다익스트라 알고리즘을 수행
-dijkstra(start)
+dijkstra(c)
 # 모든 노드로 가기 위한 최단 거리를 출력
-for i in range(1, n + 1):
+count = 0
+max_distance = 0
+for d in distance:
 # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
-    if distance[i] == INF:
-        print("INFINITY")
-    # 도달할 수 있는 경우 거리를 출력
-    else:
-        print(distance[i])
+    if d != INF:
+        count += 1
+        max_distance = max(max_distance, d)
+print(count-1, max_distance)
