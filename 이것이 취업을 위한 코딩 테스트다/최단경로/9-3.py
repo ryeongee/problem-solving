58f0b950cd7a6240170a60ea7f7dc39e23372d82
ORG <47654977+ryeongee@users.noreply.github.com>
2021-04-06 23:04:47 +0900

    .

--- 이것이 취업을 위한 코딩 테스트다/최단경로/9-3.py
+++ 이것이 취업을 위한 코딩 테스트다/최단경로/9-3.py
@@ -7,7 +7,7 @@ graph = [[INF] * (n + 1) for _ in range(n + 1)]
 # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
 for a in range(1, n + 1):
     for b in range(1, n + 1):
-        if a = = b:
+        if a == b:
             graph[a][b] = 0
 # 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
 for _ in range(m):
