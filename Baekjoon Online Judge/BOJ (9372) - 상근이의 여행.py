bb9cc59b6bd8dda24bbb10107356f30011c980b7
ORG <47654977+ryeongee@users.noreply.github.com>
2021-04-10 20:34:54 +0900

    4월 2주차

---
+++ Baekjoon Online Judge/BOJ (9372) - 상근이의 여행.py
@@ -0,0 +1,40 @@
+# https://www.acmicpc.net/problem/9372
+import sys
+input = sys.stdin.readline
+t = int(input())
+for i in range(t):
+    n,m = map(int,input().split())
+    for _ in range(m):
+        input()
+    print(n-1)
+# import sys
+# input = sys.stdin.readline
+# INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+# t = int(input())
+# for i in range(t):
+#     # 노드의 개수 및 간선의 개수를 입력받기
+#     n,m = map(int,input().split())
+#     # 2차원 리스트(그래프 표현)를 만들고, 모든 값을 무한으로 초기화
+#     graph = [[INF] * (n + 1) for _ in range(n + 1)]
+#     # 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+#     for _ in range(m):
+#         # A에서 B로 가는 비용은 C라고 설정
+#         a, b = map(int, input().split())
+#         graph[a][b] = 1
+#         graph[b][a] = 1
+#     # 점화식에 따라 플로이드 워셜 알고리즘을 수행
+#     for k in range(1, n + 1):
+#         for a in range(1, n + 1):
+#             for b in range(1, n + 1):
+#                 graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+#     # 수행된 결과를 출력
+#     cnt = 0
+#     for a in range(1, n + 1):
+#         mi = INF
+#         for b in range(1, n + 1):
+#             if a == b:
+#                 graph[a][b] == 0
+#             if graph[a][b]!= 0 and graph[a][b] < mi:
+#                 mi = graph[a][b]
+#         cnt += mi
+#     print(cnt-1)
