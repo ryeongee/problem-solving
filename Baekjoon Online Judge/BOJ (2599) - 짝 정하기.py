8fc731420e31e77a2d78f7860c99ae99add5b3c0
ORG <47654977+ryeongee@users.noreply.github.com>
2021-04-11 12:00:24 +0900

    .

---
+++ Baekjoon Online Judge/BOJ (2599) - 짝 정하기.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/2599
+# 6
+# 4 2
+# 1 3
+# 1 1
+n = int(input())
+stuList = []
+menA, womA = stuList.append(list(map(int,input().split())))
+menB, womB = stuList.append(list(map(int,input().split())))
+menC, womC = stuList.append(list(map(int,input().split())))
+
+def function(lst, n){
+    for col in range(3):
+        for raw in range(2):
+
+}
+
+function(stuList,n)
