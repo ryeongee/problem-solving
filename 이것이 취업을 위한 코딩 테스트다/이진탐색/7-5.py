80dd8a5e08d912b085bf01eab430e5c7fdd8a832
ORG <47654977+ryeongee@users.noreply.github.com>
2021-05-22 01:26:13 +0900

    Update 7-5.py

--- 이것이 취업을 위한 코딩 테스트다/이진탐색/7-5.py
+++ 이것이 취업을 위한 코딩 테스트다/이진탐색/7-5.py
@@ -2,7 +2,9 @@
 def binary_search(array, target, start, end):
 while start <= end:
 mid = (start + end) // 2
+
 # 찾은 경우 중간점 인덱스 반환
+
 if array[mid] = = target:
 return mid;
 # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
