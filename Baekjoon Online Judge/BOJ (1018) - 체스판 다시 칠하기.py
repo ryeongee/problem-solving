b79a4f963cdf72266f8e61f5c45de4749714a7b2
ORG <47654977+ryeongee@users.noreply.github.com>
2021-06-30 10:34:07 +0900

    update BOJ (1018) - 체스판 다시 칠하기.py

--- Baekjoon Online Judge/BOJ (1018) - 체스판 다시 칠하기.py
+++ Baekjoon Online Judge/BOJ (1018) - 체스판 다시 칠하기.py
@@ -1,75 +1,42 @@
-"""받아오기"""
-N,M = map(int,(input().split()))
+"""Input"""
+N, M = map(int, input().split())
 board = []
 for i in range(N):
     board.append(list(input()))
-"""정답설정"""
-for i in range(0,8):
-    if i%2==0:
-       ans1.append("BWBWBWBW")
-       ans2.append("WBWBWBWB")#정답이될 체스판
-    else:
-       ans1.append("WBWBWBWB")
-       ans2.append("BWBWBWBW")
-c_board = []
-s_min = 99999
-if N == 8 and M == 8:
-    c_ex = board
-    result = 0
-    for x in range(8):
-        stack = [c_ex[x][0]]
-        for y in range(1,8):
-            if c_ex[x][y] != stack[-1]:
-                stack.append(c_ex[x][y])
-        result += (8-len(stack)+1)//2
-        if result >= s_min:
-            break
-    if result < s_min:
-        s_min = result
+"""Setting answer"""
+ans1 = [["W", "B", "W", "B", "W", "B", "W", "B"],
+        ["B", "W", "B", "W", "B", "W", "B", "W"],
+        ["W", "B", "W", "B", "W", "B", "W", "B"],
+        ["B", "W", "B", "W", "B", "W", "B", "W"],
+        ["W", "B", "W", "B", "W", "B", "W", "B"],
+        ["B", "W", "B", "W", "B", "W", "B", "W"],
+        ["W", "B", "W", "B", "W", "B", "W", "B"],
+        ["B", "W", "B", "W", "B", "W", "B", "W"]]
 
-elif N == 8:
-        for j in range(M-8):
-            c_ex = [row[j:j+8] for row in board[:]]
-            result = 0
-            for x in range(8):
-                stack = [c_ex[x][0]]
-                for y in range(1,8):
-                    if c_ex[x][y] != stack[-1]:
-                        stack.append(c_ex[x][y])
-                result += (8-len(stack)+1)//2
-                if result >= s_min:
-                    break
-            if result < s_min:
-                s_min = result
-
-elif M == 8:
-    for i in range(N-8):
-            c_ex = [row[:] for row in board[i:i+8]]
-            result = 0
-            for x in range(8):
-                stack = [c_ex[x][0]]
-                for y in range(1,8):
-                    if c_ex[x][y] != stack[-1]:
-                        stack.append(c_ex[x][y])
-                result += (8-len(stack)+1)//2
-                if result >= s_min:
-                    break
-            if result < s_min:
-                s_min = result
-
-else:
-    for i in range(N-8):
-        for j in range(M-8):
-            c_ex = [row[j:j+8] for row in board[i:i+8]]
-            result = 0
-            for x in range(8):
-                stack = [c_ex[x][0]]
-                for y in range(1,8):
-                    if c_ex[x][y] != stack[-1]:
-                        stack.append(c_ex[x][y])
-                result += (8-len(stack))//2
-                if result >= s_min:
-                    break
-            if result < s_min:
-                s_min = result
-print(s_min)
+ans2 = [["B", "W", "B", "W", "B", "W", "B", "W"],
+        ["W", "B", "W", "B", "W", "B", "W", "B"],
+        ["B", "W", "B", "W", "B", "W", "B", "W"],
+        ["W", "B", "W", "B", "W", "B", "W", "B"],
+        ["B", "W", "B", "W", "B", "W", "B", "W"],
+        ["W", "B", "W", "B", "W", "B", "W", "B"],
+        ["B", "W", "B", "W", "B", "W", "B", "W"],
+        ["W", "B", "W", "B", "W", "B", "W", "B"]]
+"""Main"""
+min_cnt = 1000
+for i in range(N-7):
+    for j in range(M-7):
+        board2 = [row[j:j + 8] for row in board[i:i + 8]]  # Slicing chess board to 8*8
+        cnt1, cnt2 = 0, 0
+        for a in range(8):
+            for b in range(8):
+                if ans1[a][b] != board2[a][b]:
+                    cnt1 += 1
+                if ans2[a][b] != board2[a][b]:
+                    cnt2 += 1
+                else:
+                    pass
+        temp = min(cnt1, cnt2)
+        if min_cnt > temp:
+            min_cnt = temp
+"""Output"""
+print(min_cnt)
