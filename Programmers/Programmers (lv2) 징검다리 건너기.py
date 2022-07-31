91e76e96299e985b4ea83aff44e4c52ae654e03c
ORG <47654977+ryeongee@users.noreply.github.com>
2022-07-31 22:17:22 +0900

    .

---
+++ Programmers/Programmers (lv2) 징검다리 건너기.py
@@ -0,0 +1,20 @@
+def solution(stones, k):
+    left = 1
+    right = 200000000
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+        for stone in stones:
+            if stone - mid <= 0:
+                cnt += 1
+            else:
+                cnt = 0
+            if cnt >= k:
+                break
+        if cnt >= k:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return answer
