bd6773a6feda22caec630fbb736d560c8cf189cc
ORG <47654977+ryeongee@users.noreply.github.com>
2022-08-23 16:48:41 +0900

    Programmers lv3 등굣길
    Programmers lv3 등굣길

---
+++ Programmers/Programmers lv3 등굣길.py
@@ -0,0 +1,18 @@
+DIV_NUM = 1000000007
+def solution(m, n, puddles):
+    dp = [[0]*(m+1) for _ in range(n+1)]
+    dp[1][1] = 1
+    for i, j in puddles:
+        dp[j][i] = -1
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if dp[i][j] == -1:
+                dp[i][j] = 0
+                continue
+            dp[i][j] += (dp[i-1][j] + dp[i][j-1])
+    if dp[n][m] >= DIV_NUM:
+        dp[n][m] %= DIV_NUM
+    return dp[n][m]
+
+print(solution(4, 3, [[2,2]]))
