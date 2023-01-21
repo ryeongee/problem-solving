b6e5c47a9ef1ba23e21947d3256afbf76026a431
ryeongee <ryeongee24@gmail.com>
2023-01-21 23:58:23 +0900

    Update code BFS

---
+++ PythonAlgorithmInterview/BFS/BFSTest.java
@@ -0,0 +1,53 @@
+package PythonAlgorithmInterview.BFS;
+
+import java.util.*;
+import java.util.stream.Stream;
+import java.io.*;
+
+public class BFSTest {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputArr1 = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = inputArr1[0];
+        int m = inputArr1[1];
+        int v = inputArr1[2];
+
+        int[][] adjacencyMatrix = new int[n + 1][n + 1];
+        
+        for (int i = 0; i < m; i++) {
+            int[] inputArr = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            adjacencyMatrix[inputArr[0]][inputArr[1]] = 1;
+            adjacencyMatrix[inputArr[1]][inputArr[0]] = 1;
+        }
+        BFS bfs = new BFS(n, v, adjacencyMatrix);
+
+    }
+
+}
+/**
+ * BFS - 인접행렬을 이용한 방식으로 구현
+ * 구현이 빠르다. 
+ * 탐색 시 인접 리스트 방식에 비해 빠르다.  
+ * 탐색 시간복잡도 : 인접 리스트 = O(v) 인접 행렬 = O(1)
+ */
+class BFS {
+    public BFS(int n, int v, int[][] adjacencyMatrix) {
+        boolean[] visited = new boolean[n + 1];
+        int[][] adjMatrix = adjacencyMatrix;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        visited[v] = true;
+
+        while(!queue.isEmpty()){
+            v = queue.poll();
+            System.out.print(v + " ");
+            for (int i = 1; i <= n; i++) {
+                if (adjMatrix[v][i] == 1 && !visited[i]) {
+                    queue.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+}
