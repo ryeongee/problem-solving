15c3d8603b92ba2b527596c60c0167a828403977
ryeongee <ryeongee24@gmail.com>
2023-02-09 11:13:56 +0900

    Update BOJ1992.java

--- Baekjoon Online Judge/BOJ1992.java
+++ Baekjoon Online Judge/BOJ1992.java
@@ -9,46 +9,45 @@
 public class BOJ1992 {
     private static int N;
     private static int[][] IMAGE;
-    private static LinkedList<String> QUAD_TREE;
-    private static String quad;
+    private static String RESULT = "";
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        IMAGE = new int[N][N];
         for (int i = 0; i < N; i++) {
-            String column = br.readLine();
+            String[] column = br.readLine().split("");
             for (int j = 0; j < N; j++) {
-                IMAGE[i][j] = column.charAt(j) - '0';                
+                IMAGE[i][j] = Integer.parseInt(column[j]);                
             }
         }
-        QUAD_TREE = new LinkedList<>();
-        divideAndConquer(0, 0, N);
-
+        compress(0, 0, N);
+        System.out.println(RESULT);
     }
-    private static void divideAndConquer(int startX, int startY, int n){
+    private static void compress(int startX, int startY, int n){
         if(n == 1){
-            quad += IMAGE[startX][startY];
+            RESULT += IMAGE[startX][startY];
         } else {
             boolean flag = true;
             int tmp = IMAGE[startX][startY];
             Loop1:
             for (int i = startX; i < startX + n; i++) {
                 for (int j = startY; j < startY + n; j++) {
-                    if(tmp == IMAGE[i][j] && n > 1){
+                    if(tmp != IMAGE[i][j] && n > 1){
                         flag = false;
                         int tmpN = n / 2;
-                        quad += "(";
-                        divideAndConquer(startX, startY, tmpN);
-                        divideAndConquer(startX + tmpN, startY, tmpN);
-                        divideAndConquer(startX, startY + tmpN, tmpN);
-                        divideAndConquer(startX + tmpN, startY + tmpN, tmpN);
-                        quad += ")";
+                        RESULT += "(";
+                        compress(startX, startY, tmpN);
+                        compress(startX, startY + tmpN, tmpN);
+                        compress(startX + tmpN, startY, tmpN);
+                        compress(startX + tmpN, startY + tmpN, tmpN);
+                        RESULT += ")";
                         break Loop1;
                     }
                 }
             }
             if(flag){
-                quad += IMAGE[startX][startY];
+                RESULT += IMAGE[startX][startY];
             }
         }
     } 
