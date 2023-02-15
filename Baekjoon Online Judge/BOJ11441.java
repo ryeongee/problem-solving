aabe2cd2dd78de9de89142fee579283c0aeb9ba8
ryeongee <ryeongee24@gmail.com>
2023-02-15 20:44:30 +0900

    Create BOJ11441.java

---
+++ Baekjoon Online Judge/BOJ11441.java
@@ -0,0 +1,52 @@
+/**
+ * https://www.acmicpc.net/problem/11441
+ * 백준 11441 합 구하기
+5
+10 20 30 40 50
+5
+1 3
+2 4
+3 5
+1 5
+4 4
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ11441{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] prefixSum = new int[n];
+        String[] strItems = br.readLine().split(" ");
+        int m = Integer.parseInt(br.readLine());
+
+        int[] items = makePrefixSum(strItems, n);
+        for (int i = 0; i < m; i++) {
+            String[] column = br.readLine().split(" ");
+            int[] range = new int[2];
+            for (int j = 0; j < 2; j++) {
+                range[j] = Integer.parseInt(column[j]);
+            }
+            prefixSum[i] = items[range[1]] - items[range[0] - 1]; 
+        }
+
+        for (int item : prefixSum) {
+            System.out.println(item);
+        }
+    }
+    public static int[] makePrefixSum(String[] strItems, int n){
+        int[] items = new int[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(strItems[i]);
+            items[i] = sum;
+        }
+        return items;
+    }
+
+    public static int getPrefixSum(int[] items, int start, int end){
+        return items[end] - items[start - 1]; 
+    }
+}
