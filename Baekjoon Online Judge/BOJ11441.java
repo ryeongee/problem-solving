bbaf5d7b6b48f480ba6df9b6bf5eb3259719f298
ryeongee <ryeongee24@gmail.com>
2023-02-16 12:56:54 +0900

    Update BOJ11441.java

--- Baekjoon Online Judge/BOJ11441.java
+++ Baekjoon Online Judge/BOJ11441.java
@@ -1,14 +1,7 @@
 /**
  * https://www.acmicpc.net/problem/11441
  * 백준 11441 합 구하기
-5
-10 20 30 40 50
-5
-1 3
-2 4
-3 5
-1 5
-4 4
+ * 결과값 StringBuiler vs array = 724 ms : 1492 ms
  */
 
 import java.util.*;
@@ -22,38 +15,36 @@ public class BOJ11441{
         String[] strItems = br.readLine().split(" ");
         int m = Integer.parseInt(br.readLine());
 
-        int[] prefixSum = new int[n];
+        // int[] prefixSum = new int[m];
         int[] items = makePrefixSum(strItems, n);
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             String[] column = br.readLine().split(" ");
             int[] range = new int[2];
             for (int j = 0; j < 2; j++) {
                 range[j] = Integer.parseInt(column[j]);
-            }
-            System.out.println("st : "+ range[0] + " end : "+range[1]);
-            prefixSum[i] = getPrefixSum(items, range[0], range[1]);
+            }            
+            // prefixSum[i] = getPrefixSum(items, range[0], range[1]);
+            sb.append((items[range[1]] - items[range[0] - 1]) + "\n");
         }
 
-        for (int item : prefixSum) {
-            System.out.println(item);
-        }
+        // for (int item : prefixSum) {
+        //     System.out.println(item);
+        // }
+        System.out.println(sb.toString());
     }
 
     public static int[] makePrefixSum(String[] strItems, int n){
-        int[] items = new int[n];
+        int[] items = new int[n + 1];
         int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Integer.parseInt(strItems[i]);
+        for (int i = 1; i < n + 1; i++) {
+            sum += Integer.parseInt(strItems[i - 1]);
             items[i] = sum;
         }
         return items;
     }
 
     public static int getPrefixSum(int[] items, int start, int end){
-        if(start == 1){
-            return items[end - 1];
-        } else{
-            return items[end - 1] - items[start - 2]; 
-        }
+        return items[end] - items[start - 1]; 
     }
 }
