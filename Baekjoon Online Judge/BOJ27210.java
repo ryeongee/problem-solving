71d38062c8944c194da8893a0d2e93282334de00
ryeongee <ryeongee24@gmail.com>
2023-02-17 22:54:27 +0900

    Update BOJ27210.java

--- Baekjoon Online Judge/BOJ27210.java
+++ Baekjoon Online Judge/BOJ27210.java
@@ -20,17 +20,8 @@ public class BOJ27210 {
                 prefix[i] = prefix[i - 1] - 1;
             }
         }
-
-        int answer = 0;
-        for (int i = 1; i < n + 1; i++) {
-            if (prefix[i] < 0){
-                prefix[i] *= -1; 
-            }
-            if(answer < prefix[i]){
-                answer = prefix[i];
-            }
-        }
-
-        System.out.println(answer);
+        Arrays.sort(prefix);
+        int answer = prefix[n] - prefix[0];
+        System.out.println(Math.abs(answer));
     }    
 }
